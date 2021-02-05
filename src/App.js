ac50149418cba4b4d09fe291f737a29300edfe45
Tony Ngo <tonycngo@gmail.com>
2021-02-05 14:26:28 +0000

    Changed app text from feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Easy there feller!
+          Easy there feller!  I have moved to feature branch!
         </p>
         <a
           className="App-link"
